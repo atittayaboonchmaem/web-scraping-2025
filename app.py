e95dd466102a9ee02f223fbd37ecc8851a587139
atittayaboonchmaem <artitaya.11244@gmail.com>
2025-08-27 14:56:28 +0700

    เพิ่ม requirements.txt ใน app.py

--- app.py
+++ app.py
@@ -1,12 +1,126 @@
 import streamlit as st
-
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt 
-import seaborn as sns
-from sklearn.cluster import KMeans 
-from sklearn.metrics import silhouette_score
-from sklearn.preprocessing import MinMaxScaler
 
-iris = pd.read_csv("IRIS.csv")
-x = iris.iloc[:, [0, 1, 2, 3]].values
+#st.write("สวัสดี อาทิตยา")
+
+#df = pd.DataFrame({
+#    'first column': [1, 2, 3, 4],
+#    'second column': [10, 20, 30, 40]
+
+#})
+#df
+
+#import streamlit as st
+#import numpy as np
+#import pandas as pd
+
+#chart_data = pd.DataFrame(
+#     np.random.randn(20, 3),
+#     columns=['a', 'b', 'c'])
+
+#st.line_chart(chart_data)
+
+#import streamlit as st
+#import numpy as np
+#import pandas as pd
+
+#map_data = pd.DataFrame(
+#    np.random.randn(1000, 2) / [50, 50] + [19.19203, 99.87883],
+#    columns=['lat', 'lon'])
+
+#st.map(map_data)
+
+#import streamlit as st
+
+#x = st.slider('x')  # 👈 this is a widget
+#st.write(x, 'squared is', x * x)
+
+#import streamlit as st
+
+#st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+#st.session_state.name
+
+#import streamlit as st
+#import numpy as np
+#import pandas as pd
+
+#if st.checkbox('Show dataframe'):
+#    chart_data = pd.DataFrame(
+#       np.random.randn(20, 3),
+#       columns=['a', 'b', 'c'])
+
+#    chart_data
+
+#import streamlit as st
+#import pandas as pd
+
+#df = pd.DataFrame({
+#    'first column': [1, 2, 3, 4],
+#    'second column': [10, 20, 30, 40]
+#    })
+
+#option = st.selectbox(
+#    'Which number do you like best?',
+#     df['first column'])
+
+#'You selected: ', option
+
+#import streamlit as st
+
+# Add a selectbox to the sidebar:
+#add_selectbox = st.sidebar.selectbox(
+#    'How would you like to be contacted?',
+#    ('Email', 'Home phone', 'Mobile phone')
+#)
+
+# Add a slider to the sidebar:
+#add_slider = st.sidebar.slider( #sidebarกำหนดที่อยู่ว่าอยู่ส่วนไหนของหน้าเว็บ
+#    'Select a range of values',
+#    0.0, 100.0, (25.0, 75.0)
+#)
+
+#import streamlit as st
+#กำหนดว่าให้อยู่ฝั่งซ้ายของหน้าเว็บ
+#left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+#left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+#with right_column:
+#    chosen = st.radio(
+#        'Sorting hat',
+#        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+#    st.write(f"You are in {chosen} house!")
+
+#import streamlit as st
+#import time
+
+#'Starting a long computation...'
+
+# Add a placeholder
+#latest_iteration = st.empty()
+#bar = st.progress(0)
+
+#for i in range(100):
+# Update the progress bar with each iteration.
+#  latest_iteration.text(f'Iteration {i+1}')
+#  bar.progress(i + 1)
+#  time.sleep(0.1)
+
+#'...and now we\'re done!'
+
+#if "counter" not in st.session_state:
+#    st.session_state.counter = 0
+
+#st.session_state.counter += 1
+
+#st.header(f"This page has run {st.session_state.counter} times.")
+#st.button("Run it again")
+
+import streamlit as st
+
+conn = st.connection("my_database")
+df = conn.query("select * from film")
+st.dataframe(df)
